5519df2c87a79d2d522b6ce74912f9b7477bb97c
Luthetus <luthetus@gmail.com>
2026-02-12 17:40:35 -0500

    button positioning

--- TextEditor.ServerSide/Lists/ListComponent.razor.js
+++ TextEditor.ServerSide/Lists/ListComponent.razor.js
@@ -26,10 +26,21 @@
             let relativeY = event.clientY - parentBoundingClientRect.top;
             let indexClicked = -1;
             for (let i = 0; i < listChildrenContainerElement.children.length; i++) {
-                let childBoundingClientRect = listChildrenContainerElement.children[i].getBoundingClientRect();
+                let childElement = listChildrenContainerElement.children[i];
+                let childBoundingClientRect = childElement.getBoundingClientRect();
                 let relativeChildTop = childBoundingClientRect.top - parentBoundingClientRect.top;
                 if (relativeY >= relativeChildTop && relativeY < relativeChildTop + childBoundingClientRect.height) {
-                    indexClicked = i;
+                    // If the state is invalid a div with solely a text node is displayed.
+                    // ... I'm not sure whether a text node would be in the '.children' property.
+                    if (childElement.children.length > 0) {
+                        let buttonElement = childElement.children[childElement.children.length - 1];
+                        let buttonBoundingClientRect = buttonElement.getBoundingClientRect();
+                        if (event.clientX >= buttonBoundingClientRect.left && event.clientX < buttonBoundingClientRect.left + buttonBoundingClientRect.width &&
+                            event.clientY >= buttonBoundingClientRect.top && event.clientY < buttonBoundingClientRect.top + buttonBoundingClientRect.height) {
+
+                            indexClicked = i;
+                        }
+                    }
                     break;
                 }
             }
