da3dd12f608ac5bc82a1d509a3135ad515ff142e
Luthetus <luthetus@gmail.com>
2026-02-12 20:16:24 -0500

    hmm

--- TextEditor.ServerSide/Lists/ListComponent.razor.js
+++ TextEditor.ServerSide/Lists/ListComponent.razor.js
@@ -26,11 +26,12 @@
     }
 
     getListVirtualizationRequest() {
-        let listChildrenContainerElement = document.getElementById(this.htmlId);
+        let listElement = document.getElementById(this.htmlId);
+        if (!listElement || listElement.children.length != this.countWellknownImmediateElements) return;
 
         return {
-            Skip: Math.floor(listChildrenContainerElement.scrollTop / this.itemHeight),
-            Take: Math.ceil(listChildrenContainerElement.offsetHeight / this.itemHeight),
+            Skip: Math.floor(listElement.scrollTop / this.itemHeight),
+            Take: Math.ceil(listElement.offsetHeight / this.itemHeight),
         };
     }
 
@@ -60,35 +61,28 @@
     setItemHeight(itemHeight) {
         this.itemHeight = itemHeight;
 
-        let listChildrenContainerElement = document.getElementById(this.htmlId);
-        if (!listChildrenContainerElement) return;
+        let listElement = document.getElementById(this.htmlId);
+        if (!listElement || listElement.children.length != this.countWellknownImmediateElements) return;
 
-        if (listChildrenContainerElement.children.length == 0) return;
-
-        let cursorElement = listChildrenContainerElement.children[0];
+        let cursorElement = listElement.children[this.indexCursorImmediateElement];
         cursorElement.style.height = this.itemHeight + "px";
     }
 
     registerHandles() {
-        let listChildrenContainerElement = document.getElementById(this.htmlId);
-        if (!listChildrenContainerElement) {
+        let listElement = document.getElementById(this.htmlId);
+        if (!listElement || listElement.children.length != this.countWellknownImmediateElements) {
             this.initializedSuccessfully = false;
             return;
         }
 
-        let cursorElement = null;
-        if (listChildrenContainerElement.children.length == 0) {
-            this.initializedSuccessfully = false;
-            return;
-        }
-        cursorElement = listChildrenContainerElement.children[0];
+        let cursorElement = listElement.children[this.indexCursorImmediateElement];
 
         let measureLineHeightElement = document.createElement('div');
         // This is a copy and pasted example from dev tools of just a text render fragment that was using C#:'Path.GetRandomFileName()' twice to generate text.
         measureLineHeightElement.innerHTML = "wwwe3w5r.vgc jmfgv034.wno<button class=\"btn btn-danger\">Delete</button>";
-        listChildrenContainerElement.appendChild(measureLineHeightElement);
+        listElement.appendChild(measureLineHeightElement);
         this.setItemHeight(Math.ceil(measureLineHeightElement.getBoundingClientRect().height));
-        listChildrenContainerElement.removeChild(measureLineHeightElement);
+        listElement.removeChild(measureLineHeightElement);
         if (this.itemHeight < 1) {
             this.initializedSuccessfully = false;
             return;
@@ -96,12 +90,12 @@
 
         cursorElement.className = 'list-component-cursor';
 
-        listChildrenContainerElement.addEventListener('click', event => {
-            let parentBoundingClientRect = listChildrenContainerElement.getBoundingClientRect();
+        listElement.addEventListener('click', event => {
+            let parentBoundingClientRect = listElement.getBoundingClientRect();
             let relativeY = event.clientY - parentBoundingClientRect.top;
             let indexClicked = -1;
-            for (let i = 0; i < listChildrenContainerElement.children.length; i++) {
-                let childElement = listChildrenContainerElement.children[i];
+            for (let i = 0; i < listElement.children.length; i++) {
+                let childElement = listElement.children[i];
                 let childBoundingClientRect = childElement.getBoundingClientRect();
                 let relativeChildTop = childBoundingClientRect.top - parentBoundingClientRect.top;
                 if (relativeY >= relativeChildTop && relativeY < relativeChildTop + childBoundingClientRect.height) {
@@ -122,7 +116,7 @@
             this.dotNetObjectReference.invokeMethodAsync("OnClick", indexClicked);
         });
 
-        listChildrenContainerElement.addEventListener('keydown', event => {
+        listElement.addEventListener('keydown', event => {
             switch (event.key) {
                 case 'ArrowDown':
                     event.preventDefault();
