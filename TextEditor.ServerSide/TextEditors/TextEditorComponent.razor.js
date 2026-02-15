4b307bae90a354fcacdf99e3b7b7986bb059095f
Luthetus <luthetus@gmail.com>
2026-02-15 17:13:54 -0500

    virtual line 10/10 mouth feel

--- TextEditor.ServerSide/TextEditors/TextEditorComponent.razor.js
+++ TextEditor.ServerSide/TextEditors/TextEditorComponent.razor.js
@@ -68,7 +68,7 @@ export class TextEditor {
                 cursor.gapElement.style.left = 0;
                 cursor.gapElement.style.top = 0;
 
-                // TODO: If you same line multicursor you need to share the fabricated line
+                // TODO: If you same line multicursor you need to share the virtual line
                 cursor.lineIndex = 0;
             }
             this.editorElement.appendChild(cursor.gapElement);
