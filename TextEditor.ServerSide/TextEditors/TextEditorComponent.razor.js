a090bb9ac52dbd3a80dc51851c9ca3afc72e3b63
Luthetus <luthetus@gmail.com>
2026-02-15 16:41:03 -0500

    good

--- TextEditor.ServerSide/TextEditors/TextEditorComponent.razor.js
+++ TextEditor.ServerSide/TextEditors/TextEditorComponent.razor.js
@@ -144,10 +144,12 @@ export class TextEditor {
             // FURTHERMORE: I need to revisit calcuating the character width, this is somewhat of an early
             // way I found to get it, perhaps it isn't quite so involved.
             //
-            // This is '7.8813131313131315'
-            // sampleTextBuilder.push("abcdefghijklmnopqrstuvwxyz123456789");
-            // this is 100.45... --- now it is 8.909090909090908
-            sampleTextBuilder.push("a");
+            // This is '7.8813131313131315' ---
+            // now is  '7.8813131313131315'
+            sampleTextBuilder.push("abcdefghijklmnopqrstuvwxyz123456789");
+            // this is '100.45...'
+            // now is  '8.909090909090908'
+            // sampleTextBuilder.push("a");
         }
         // TODO: What does 'const' mean.
         measureElement.innerHTML = sampleTextBuilder.join("");
@@ -156,8 +158,8 @@ export class TextEditor {
         // It might measure as the width of the parent in this case.
         // fit-content?
 
-        this.characterWidth = measureElement.offsetWidth / (1 * 11);
-        //this.characterWidth = measureElement.offsetWidth / (36 * 11);
+        //this.characterWidth = measureElement.offsetWidth / (1 * 11);
+        this.characterWidth = measureElement.offsetWidth / (36 * 11);
         this.lineHeight = measureElement.offsetHeight;
 
         this.editorElement.removeChild(measureElement);
